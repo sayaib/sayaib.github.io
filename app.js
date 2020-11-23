bf4c8e57ac31f82eb22573fa7bf50c8da2489b20
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2020-11-23 12:21:23 +0530

    update app javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+function mFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+var preload = document.getElementById("loading");
+function myFunction(){
+  preload.style.display = "none";
+}
+AOS.init();
