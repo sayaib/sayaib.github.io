c7494ac7e5fe04b1adcb4ef2e7099c378eb5394a
Sayaib Sarkar <ssayaib@gmail.com>
2021-05-02 14:48:00 +0530

    update

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ function myFunction(){
   preload.style.display = "none";
 }
 AOS.init();
+
+
+
+var date = new Date();
+var currTime = date.getHours();
+
+var greating = document.getElementById('time');
+
+if(currTime >=1 && currTime<12){
+greating.innerHTML = 'Good Morning';
+}else if(currTime>12 && currTime <=16 ){
+  greating.innerHTML = 'Good Afternoon';
+}else if(currTime>16 && currTime<=19){
+  greating.innerHTML = 'Good Evening';
+}else if(currTime>19 && currTime <24){
+  greating.innerHTML = 'Good Night';
+}
