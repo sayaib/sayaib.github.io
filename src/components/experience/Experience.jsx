0c0897701180b657a0e98236051391a55bffda18
Sayaib Sarkar <ssayaib@gmail.com>
2025-01-02 10:44:37 +0530

    update the main webiste

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BsFillPatchCheckFill } from 'react-icons/bs';
-import './experience.css';
+import React from "react";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 const Experience = () => {
   return (
@@ -9,9 +9,9 @@ const Experience = () => {
       <h2>Skills</h2>
       <div className="container experience__container">
         <div className="experience__frontend">
-          <h3>Front-end Development</h3>
+          <h3>Full-stack Development</h3>
           <div className="experience__content">
-          <article className="experience__details">
+            <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>HTML / CSS</h4>
             </article>
@@ -51,11 +51,19 @@ const Experience = () => {
               <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>Redux, Redux-toolkit, Context API</h4>
             </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>MongoDB, SQL, PostGre</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Flutter - Dart</h4>
+            </article>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,24 +1,111 @@
-import React, { useState } from 'react';
-import { AiOutlineHome } from 'react-icons/ai';
-import { AiOutlineUser } from 'react-icons/ai';
-import { BiBook } from 'react-icons/bi';
-import { RiServiceLine } from 'react-icons/ri';
-import { BiMessageSquareDetail } from 'react-icons/bi';
+// import React, { useState } from "react";
+// import { AiOutlineHome } from "react-icons/ai";
+// import { AiOutlineUser } from "react-icons/ai";
+// import { BiBook } from "react-icons/bi";
+// import { RiServiceLine } from "react-icons/ri";
+// import { BiMessageSquareDetail } from "react-icons/bi";
 
+// import "./topbar.css";
 
-import './topbar.css';
+// const Topbar = () => {
+//   const [activeNav, setActiveNav] = useState("#home");
+//   return (
+//     <nav>
+//       <a
+//         href="#home"
+//         onClick={() => setActiveNav("#home")}
+//         className={activeNav === "#home" ? "active" : ""}
+//       >
+//         <AiOutlineHome />
+//       </a>
+//       <a
+//         href="about"
+//         onClick={() => setActiveNav("#about")}
+//         className={activeNav === "#about" ? "active" : ""}
+//       >
+//         <AiOutlineUser />
+//       </a>
+//       <a
+//         href="#experience"
+//         onClick={() => setActiveNav("#experience")}
+//         className={activeNav === "#experience" ? "active" : ""}
+//       >
+//         <BiBook />
+//       </a>
+//       <a
+//         href="#portfolio"
+//         onClick={() => setActiveNav("#portfolio")}
+//         className={activeNav === "#portfolio" ? "active" : ""}
+//       >
+//         <RiServiceLine />
+//       </a>
+//       <a
+//         href="#contact"
+//         onClick={() => setActiveNav("#contact")}
+//         className={activeNav === "#contact" ? "active" : ""}
+//       >
+//         <BiMessageSquareDetail />
+//       </a>
+//     </nav>
+//   );
+// };
+
+// export default Topbar;
+
+import React from "react";
+import {
+  AiFillCodepenCircle,
+  AiFillCode,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiFillGithub,
+} from "react-icons/ai";
+
+import { VscVscode } from "react-icons/vsc";
+
+import "./topbar.css";
 
 const Topbar = () => {
-  const [activeNav, setActiveNav] = useState('#home');
   return (
     <nav>
-      <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome /></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
-      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine /></a>
-      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+      <a
+        href="https://codepen.io/sayaib"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillCodepenCircle />
+      </a>
+      <a
+        href="https://marketplace.visualstudio.com/items?itemName=sayaib.hue-console#:~:text=DevSnip%20Pro%20Full%20Documentation&text=Select%20Your%20Code%3A%20Highlight%20the,option%20from%20the%20context%20menu."
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillCode />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/sayaib/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillLinkedin />
+      </a>
+      <a
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillTwitterCircle />
+      </a>
+      <a
+        href="https://github.com/sayaib"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiFillGithub />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
 export default Topbar;
